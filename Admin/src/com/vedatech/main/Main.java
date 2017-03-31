6382a05c84d9370ea7ebe6be8559021ae95f7fb5
edilberto1960 <e.ramos.salinas@gmail.com>
2017-03-31 08:50:44 -0600

    Change-Id: I3e693481870370f32b5202b3bab7aad7c8279839 Signed-off-by: edilberto1960 <e.ramos.salinas@gmail.com>

--- Admin/src/com/vedatech/main/Main.java
+++ Admin/src/com/vedatech/main/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("nuevo codigo adherido");
+		System.out.println("nuevo codigo adherido9");
 
 	}
 
